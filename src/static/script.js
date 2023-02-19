2f311deb32494e9c70aabdc52e51494494948916
milanmilosev1 <milosevmil@gmail.com>
2023-02-19 21:13:57 +0100

    Changed the main container a bit, styled the "read more" button(problem with the button itself the pressing does not do anything even if i state that it should go to #read-more-text). Added 3 more text containers that need to be paired with banner images side-by-side. Fixed the footer not on the bottom.

---
+++ src/static/script.js
@@ -0,0 +1,26 @@
+VanillaTilt.init(document.querySelector("#read-more"), {
+  max: 10,
+  speed: 400,
+  glare: true,
+});
+
+VanillaTilt.init(document.querySelector(".banner-image"), {
+  max: 10,
+  speed: 400,
+  glare: true,
+});
+VanillaTilt.init(document.querySelector(".scnd-text"), {
+  max: 5,
+  speed: 400,
+  glare: false,
+});
+VanillaTilt.init(document.querySelector(".thrd-text"), {
+  max: 5,
+  speed: 400,
+  glare: false,
+});
+VanillaTilt.init(document.querySelector(".frth-text"), {
+  max: 5,
+  speed: 400,
+  glare: false,
+});
